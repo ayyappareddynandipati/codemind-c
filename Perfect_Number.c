cf2718cbff5e60e0b0889f602738328c79f7c79b
ayyappareddynandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2022-12-26 22:05:25 +0530

    #include<stdio.h> int main() {     int i,n,r,s=0;     scanf("%d",&n);     for(i=1;i<=(n-1);i++)     {         r=n%i;         if(r==0)         {             s=s+i;         }     }     if(n==s)     printf("True");     else     printf("False"); }

---
+++ Perfect_Number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,r,s=0;
+    scanf("%d",&n);
+    for(i=1;i<=(n-1);i++)
+    {
+        r=n%i;
+        if(r==0)
+        {
+            s=s+i;
+        }
+    }
+    if(n==s)
+    printf("True");
+    else
+    printf("False");
+}
