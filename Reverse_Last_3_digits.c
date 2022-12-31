1bd11cb9e07b7af819df1aec3cdd89fae5949d8e
ayyappareddynandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2022-12-31 18:31:22 +0530

    #include<stdio.h> int main() {     int n,m,a,r,t,c,t1;     scanf("%d",&n);     m=n%1000;     a=m;     while(m!=0)     {         r=m%10;         t=t*10+r;         m=m/10;         }         t1=n-a;         c=t1+t;         printf("%d",c);
    }

--- Reverse_Last_3_digits.c
+++ Reverse_Last_3_digits.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
 int main()
-{
+{ 
     int n,m,a,r,t,c,t1;
     scanf("%d",&n);
     m=n%1000;
     a=m;
     while(m!=0)
-    {
-        r=m%10;
+    { 
+        r=m%10; 
         t=t*10+r;
         m=m/10;
-    }
-    t1=n-a;
-    c=t1+t;
-    printf("%d",c);
+        }
+        t1=n-a;
+        c=t1+t;
+        printf("%d",c);
+    
 }
