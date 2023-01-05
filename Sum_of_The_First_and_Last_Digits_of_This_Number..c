d05ea9a345aeff01ecabba6e0fe26aaf98e1949d
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2023-01-05 23:04:28 +0530

    #include<stdio.h> #include<math.h> int main() {     int x,i;     scanf("%d",&x);     for(i=1;i<=x;i++)     {         long int n;         scanf("%lld",&n);         int r,k,f,sum,d;         d=log10(n)+1;         while(n>=0)         {             r=n%10;             break;         }         k=pow(10,(d-1));         f=n/k;         sum=r+f;         printf("%d ",sum);     }
    }

--- Sum_of_The_First_and_Last_Digits_of_This_Number..c
+++ Sum_of_The_First_and_Last_Digits_of_This_Number..c
@@ -5,20 +5,21 @@ int main()
     int x,i;
     scanf("%d",&x);
     for(i=1;i<=x;i++)
-{
-    long int n;
-    scanf("%lld",&n);
-    int r,k,f,sum,d;
-    d=log10(n)+1;
-    while(n>=0)
     {
-        r=n%10;
-        break;
-    }
-    k=pow(10,(d-1));
-    f=n/k;
-    sum=r+f;
-    printf("%d
+        long int n;
+        scanf("%lld",&n);
+        int r,k,f,sum,d;
+        d=log10(n)+1;
+        while(n>=0)
+        {
+            r=n%10;
+            break;
+        }
+        k=pow(10,(d-1));
+        f=n/k;
+        sum=r+f;
+        printf("%d
 ",sum);
-}
+    }
+
 }
