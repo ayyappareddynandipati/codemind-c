9ba1437c802ea321bd8a3161a8506916523d0325
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2023-01-21 20:31:19 +0530

    #include<stdio.h> int main() {     int n,i,j,t=0;     scanf("%d",&n);     int x[n];     for(i=0;i<n;i++)     {         scanf("%d",&x[i]);     }     for(i=0;i<n;i++)     {         for(j=i+1;j<n;j++)         {             if(x[i]>x[j])             {                 t=x[i];                 x[i]=x[j];                 x[j]=t;             }         }     }     printf("%d",x[2]);
    }

--- Calculate_The_Median.c
+++ Calculate_The_Median.c
@@ -3,23 +3,23 @@ int main()
 {
     int n,i,j,t=0;
     scanf("%d",&n);
-    int x[n]; 
+    int x[n];
     for(i=0;i<n;i++)
     {
         scanf("%d",&x[i]);
-        } 
-        for(i=0;i<n;i++)
-        { 
-            for(j=i+1;j<n;j++) 
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(x[i]>x[j])
             {
-                if(x[i]>x[j])
-                {
-                    t=x[i];
-                    x[i]=x[j];
-                    x[j]=t; 
-                }
+                t=x[i];
+                x[i]=x[j];
+                x[j]=t;
             }
         }
-        printf("%d",x[2]);
+    }
+    printf("%d",x[2]);
 
 }
