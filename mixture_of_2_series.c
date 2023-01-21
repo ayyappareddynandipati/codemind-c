dd5a93f0b773002d47c6622a69da631ecaf3ef92
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2023-01-21 20:33:13 +0530

    #include<stdio.h> int main() {     int n,i,a,b,c=0,d=-1;     scanf("%d",&n);     for(i=0;i<=n;i++)     {         if(i%2==0)         {             a=2*c;             printf("%d ",a);             c++;         }         else         {             b=d+1;             printf("%d ",b);             d++;         }     }
    }

--- mixture_of_2_series.c
+++ mixture_of_2_series.c
@@ -3,19 +3,20 @@ int main()
 {
     int n,i,a,b,c=0,d=-1;
     scanf("%d",&n);
-    for(i=0;i<=n;i++)
+    for(i=0;i<=n;i++) 
     {
-       if(i%2==0)
-       {
-           a=2*c;
-           printf("%d ",a);
-           c++;
-       }
-       else
-       {
-           b=d+1;
-           printf("%d ",b);
-           d++;
-       }
+        if(i%2==0)
+        {
+            a=2*c; 
+            printf("%d ",a);
+            c++;
+        }
+        else
+        {
+            b=d+1;
+            printf("%d ",b);
+            d++;
+        }
     }
+    
 }
