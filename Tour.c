1fa7091c76d57ddc6ae18f74e3810b3058d48016
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2023-01-25 19:17:23 +0530

    #include<stdio.h> int main() {     int a,i,s;     scanf("%d",&a);     for(i=1;i<=a;i++)     {         int n,m;         scanf("%d%d",&n,&m);         s=5*n+7*m;         printf("%d ",s);
        }
        
    }

---
+++ Tour.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,s;
+    scanf("%d",&a);
+    for(i=1;i<=a;i++) 
+    {
+        int n,m;
+        scanf("%d%d",&n,&m);
+        s=5*n+7*m;
+        printf("%d
+",s); 
+        
+    }
+    
+}
