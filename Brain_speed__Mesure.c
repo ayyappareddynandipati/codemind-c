b8ee66a105a5ba8f3fcd2fd7c5635060c2e25eff
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2023-01-25 19:14:40 +0530

    #include<stdio.h> int main() {     int x,y;     scanf("%d%d",&x,&y);     if(x<y)     printf("YES");     else     printf("NO");
    }

--- Brain_speed__Mesure.c
+++ Brain_speed__Mesure.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int x,y; 
-    scanf("%d%d",&x,&y);
-    if(x<y) 
+    int x,y;
+    scanf("%d%d",&x,&y); 
+    if(x<y)
     printf("YES");
-    else
+    else 
     printf("NO");
     
 }
