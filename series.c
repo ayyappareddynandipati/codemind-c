a8295279ae1a9af3eaa1719f157f5d91ab687ca0
Ayyappa Reddy Nandipati <119839645+ayyappareddynandipati@users.noreply.github.com>
2023-01-27 22:02:18 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,a,b,k1=0,k2=0;     scanf("%d",&n);     printf("0 ");     for(i=1;i<n;i++)     {         if(i%2!=0)         {             a=pow(3,k1);             printf("%d ",a);             k1++;         }         else         {             b=pow(2,k2);             printf("%d ",b);             k2++;         }     }
    }

--- series.c
+++ series.c
@@ -2,22 +2,23 @@
 #include<math.h>
 int main()
 {
-    int n,i,a,b,c=1,d=1;
+    int n,i,a,b,k1=0,k2=0;
     scanf("%d",&n);
-    for(i=0;i<=n;i++)
-    { 
-        if(i%2==0)
+    printf("0 "); 
+    for(i=1;i<n;i++)
+    {
+        if(i%2!=0)
         {
-            a=pow(2,c)-1;
+            a=pow(3,k1);
             printf("%d ",a);
-            c++;
+            k1++;
         }
         else
         {
-            b=pow(3,d)-1;
+            b=pow(2,k2);
             printf("%d ",b);
-            d++;
+            k2++; 
         }
     }
-    
+
 }
